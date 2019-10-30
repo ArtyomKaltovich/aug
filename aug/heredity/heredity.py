b451927721f1373fd734d2deee5876c2b7997636
Artyom Kaltovich <KaltovichArtyom@gmail.com>
2019-10-30 21:19:58 +0300

    add neighbour_joining

--- aug/heredity/heredity.py
+++ aug/heredity/heredity.py
@@ -1,5 +1,7 @@
 import copy
 import heapq
+import itertools
+from typing import Dict, Tuple
 
 from aug.heredity.Phenotype import PhenotypeHeredityTable
 
@@ -54,7 +56,6 @@ class PhylogenyTree:
             super().__init__(left, right, score, sep)
             self.size = left.size if isinstance(left, PhylogenyTree._UnweightedElem) else 1
             self.size += right.size if isinstance(right, PhylogenyTree._UnweightedElem) else 1
-            pass
 
         def recalculate_tree_score(self, dist_matrix, elem, left, right):
             left_size = 1 if type(left) is str else left.size
@@ -62,18 +63,30 @@ class PhylogenyTree:
             return (dist_matrix[(str(elem), str(left))] * left_size + dist_matrix[(str(elem), str(right))] * right_size)\
                    / (left_size + right_size)
 
+    class _NeighbourJoining(_WeightedElem):
+        def __init__(self, left=None, right=None, score=None, sep="-"):
+            super().__init__(left, right, score, sep)
+
+        def to_newick(self):
+            left = self.left if type(self.left) is str else self.left.to_newick()
+            right = self.right if type(self.right) is str else self.right.to_newick()
+            return f"({left}:{self.score[0]}, {right}:{self.score[1]})"
+
     def weighted(self):
         return PhylogenyTree._WeightedElem
 
     def unweighted(self):
         return PhylogenyTree._UnweightedElem
 
-    def __init__(self, dist_matrix, method=weighted, sep="-"):
-        """
-        :param dist_list:
-        :param name_to_index:
-        :param method:
-        :return:
+    def neighbour_joining(self):
+        return PhylogenyTree._NeighbourJoining
+
+    def __init__(self, dist_matrix: Dict[Tuple[str, str], float], method=weighted, sep="-"):
+        """ Builds Phylogeny Tree
+        Can be translated to newick format by str function
+        :param dist_matrix: matrix of distances
+        :param method: method to build tree
+        :param sep: separator between names in united tree
 
         >>> dist_matrix = {("A", "B"): 16, ("A", "C"): 16, ("A", "D"): 10,
         ...                ("B", "C"): 8, ("B", "D"): 8,
@@ -105,28 +118,78 @@ class PhylogenyTree:
         >>> str(tree)
         '(F:4.4, ((D:2.5, E:2.5):1.25, (B:3.0, (A:2.0, C:2.0):1.0):0.75):0.6500000000000004)'
         """
-        # using heapq to decrease complexity of min search
-        # 1. save all values to heapq
-        # 2. select min
-        # 3. add dists for united values to heapq
-        # 4. until there are elements in heapq go to step 2
-        # On step 2 there can be invalid value selected (distance to element, that was already united)
-        # the algorithm will keep all united values in set, and if selected minimum is for invalid elements,
-        # it will be through away.
-        # So with dist matrix we should find min O(N^2) and update row\column O(N) on every step.
-        # Now the algorithm will discard invalid values (on each step the can be only O(N) values).
-        dist_matrix, dist_list, unprocessed = self._get_initial_values_for_tree(dist_matrix)
-        method = method(self)
-        self._tree = method()
-        while len(unprocessed) > 1:
-            left, right, score = self._get_next_tree_elem(dist_list, unprocessed)
-            self._tree = method(left, right, score, sep)
-            unprocessed.discard(left)
-            unprocessed.discard(right)
-            new_elem = self._tree
-            for elem in unprocessed:
-                self._update_tree_score(dist_matrix, dist_list, elem, new_elem, left, right)
-            unprocessed.add(new_elem)
+        if method == PhylogenyTree.weighted or method == PhylogenyTree.unweighted:
+            # using heapq to decrease complexity of min search
+            # 1. save all values to heapq
+            # 2. select min
+            # 3. add dists for united values to heapq
+            # 4. until there are elements in heapq go to step 2
+            # On step 2 there can be invalid value selected (distance to element, that was already united)
+            # the algorithm will keep all united values in set, and if selected minimum is for invalid elements,
+            # it will be through away.
+            # So with dist matrix we should find min O(N^2) and update row\column O(N) on every step.
+            # Now the algorithm will discard invalid values (on each step the can be only O(N) values).
+            # While inserting is O(logN^2) and we will insert N value, so complexity of one step will be O(NlogN^2)
+            dist_matrix, dist_list, unprocessed = self._get_initial_values_for_tree(dist_matrix)
+            method = method(self)
+            self._tree = method()
+            while len(unprocessed) > 1:
+                left, right, score = self._get_next_tree_elem(dist_list, unprocessed)
+                self._tree = method(left, right, score, sep)
+                unprocessed.discard(left)
+                unprocessed.discard(right)
+                new_elem = self._tree
+                for elem in unprocessed:
+                    self._update_tree_score(dist_matrix, dist_list, elem, new_elem, left, right)
+                unprocessed.add(new_elem)
+        elif method == PhylogenyTree.neighbour_joining:
+            unprocessed = self._get_names_set_from_dist_matrix(dist_matrix)
+            name_to_index = {}
+            index_to_name = []
+            for i, key in enumerate(unprocessed):
+                name_to_index[key] = i
+                index_to_name.append(key)
+            unprocessed = set(range(len(index_to_name)))
+            matrix = [[0] * len(name_to_index) for _ in range(len(name_to_index))]
+            for (left, right), score in dist_matrix.items():
+                i, j = name_to_index[left], name_to_index[right]
+                matrix[i][j] = score
+                matrix[j][i] = score
+
+            method = method(self)
+            self._tree = method()
+
+            for n in range(len(matrix), 1, -1):
+                dist_sums = [sum(row) for row in matrix]
+                min_ = [None, None, None, None, None]  # score, i, j, left_score, right_score
+                for i, j in itertools.combinations(unprocessed, 2):
+                    score = matrix[i][j] - (dist_sums[i] + dist_sums[j] - 2 * matrix[i][j]) / (n - 2) if n > 2 else matrix[i][j]
+                    if min_[0] is None or score < min_[0]:
+                        min_[0] = score
+                        min_[1] = i
+                        min_[2] = j
+                        temp = (dist_sums[i] - dist_sums[j]) / (n - 2) if n > 2 else 0
+                        min_[3] = (matrix[i][j] + temp) / 2
+                        min_[4] = (matrix[i][j] - temp) / 2
+                left = min_[1]
+                right = min_[2]
+                self._tree = method(index_to_name[left], index_to_name[right], (min_[3], min_[4]), sep)
+
+                name_to_index[str(self._tree)] = left
+                index_to_name[left] = self._tree
+                unprocessed.discard(right)
+                m = copy.deepcopy(matrix)
+                for k in unprocessed - {left}:
+                    score = (matrix[left][k] + matrix[right][k] - matrix[left][right]) / 2
+                    m[left][k] = score
+                    m[k][left] = score
+                matrix = m
+                for k in range(len(matrix)):
+                    matrix[right][k] = 0
+                    matrix[k][right] = 0
+                pass
+        else:
+            raise ValueError("Unsupported method!")
 
     def _update_tree_score(self, dist_matrix, dist_list, elem, new_elem, left, right):
         elem_str = str(elem)
@@ -145,13 +208,17 @@ class PhylogenyTree:
     def _get_initial_values_for_tree(self, dist_matrix):
         dist_matrix = copy.copy(dist_matrix)
         dist_list = list((value, key) for key, value in dist_matrix.items())
+        unprocessed = self._get_names_set_from_dist_matrix(dist_matrix)
+        heapq.heapify(dist_list)
+        dist_matrix.update({(r, l): score for (l, r), score in dist_matrix.items()})
+        return dist_matrix, dist_list, unprocessed
+
+    def _get_names_set_from_dist_matrix(self, dist_matrix):
         unprocessed = set()
         for l, r in dist_matrix:
             unprocessed.add(l)
             unprocessed.add(r)
-        heapq.heapify(dist_list)
-        dist_matrix.update({(r, l): score for (l, r), score in dist_matrix.items()})
-        return dist_matrix, dist_list, unprocessed
+        return unprocessed
 
     def __str__(self):
         return self._tree.to_newick()
